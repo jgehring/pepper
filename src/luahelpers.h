de299025c072c33ab5474a5b7f97e2d3a8e90109
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-10-15 01:18:53 +0200

    More functions for LuaHelpers namespace

--- src/luahelpers.h
+++ src/luahelpers.h
@@ -20,6 +20,26 @@
 namespace LuaHelpers
 {
 
+inline int push(lua_State *L, int i) {
+	lua_pushinteger(L, i);
+	return 1;
+}
+
+inline int push(lua_State *L, double i) {
+	lua_pushnumber(L, i);
+	return 1;
+}
+
+inline int push(lua_State *L, bool i) {
+	lua_pushboolean(L, (int)i);
+	return 1;
+}
+
+inline int push(lua_State *L, const char *s) {
+	lua_pushstring(L, s);
+	return 1;
+}
+
 inline int push(lua_State *L, const std::string &s) {
 	lua_pushstring(L, s.c_str());
 	return 1;
@@ -37,6 +57,27 @@ inline int push(lua_State *L, const std::vector<std::string> &v) {
 
 inline int pushError(lua_State *L, const std::string &e) {
 	return luaL_error(L, e.c_str());
+}
+
+template<typename T1>
+inline void call(lua_State *L, const T1 &arg1, int nresults) {
+	push(L, arg1);
+	lua_call(L, 1, nresults);
+}
+
+template<typename T1, typename T2>
+inline void call(lua_State *L, const T1 &arg1, const T2 &arg2, int nresults) {
+	push(L, arg1);
+	push(L, arg2);
+	lua_call(L, 2, nresults);
+}
+
+template<typename T1, typename T2, typename T3>
+inline void call(lua_State *L, const T1 &arg1, const T2 &arg2, const T3 &arg3, int nresults) {
+	push(L, arg1);
+	push(L, arg2);
+	push(L, arg3);
+	lua_call(L, 3, nresults);
 }
 
 } // namespace LuaHelpers
