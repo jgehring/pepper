a721830646b92ec50a88ba898bddf332756410ac
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-10-25 13:20:13 +0200

    Fixed appending to gzipped files in BOStream

--- src/bstream.cpp
+++ src/bstream.cpp
@@ -46,13 +46,52 @@ BIStream::BIStream(const std::string &path)
 }
 
 
+// Single-linked buffer
+struct LBuffer {
+	char *data;
+	int size;
+	LBuffer *next;
+	LBuffer() : data(NULL), size(0), next(NULL) { }
+	~LBuffer() { delete[] data; }
+};
+
 // Constructor
 BOStream::BOStream(const std::string &path, bool append)
 #ifdef HAVE_LIBZ
-	: BStream(gzopen(path.c_str(), (append ? "ab" : "wb")))
+	: BStream(append ? NULL : gzopen(path.c_str(), "wb"))
 #else
 	: BStream(fopen(path.c_str(), (append ? "ab" : "wb")))
 #endif
 {
+#ifdef HAVE_LIBZ
+	// Appending to gzipped files does not work. Instead, read the whole
+	// file and re-write it.
+	if (append) {
+		LBuffer *buf = NULL, *first = NULL;
+		gzFile in = gzopen(path.c_str(), "rb");
+		if (in) {
+			while (!gzeof(in)) {
+				if (buf) {
+					buf->next = new LBuffer();
+					buf = buf->next;
+				} else {
+					buf = new LBuffer();
+					first = buf;
+				}
+				buf->data = new char[4096];
+				buf->size = gzread(in, buf->data, 4096);
+			}
+		}
 
+		gzclose(in);
+		m_file = gzopen(path.c_str(), "wb");
+		buf = first;
+		while (buf != NULL) {
+			gzwrite(m_file, buf->data, buf->size);
+			LBuffer *tmp = buf;
+			buf = buf->next;
+			delete tmp;
+		}
+	}
+#endif
 }
