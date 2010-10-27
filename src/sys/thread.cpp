af19d8b6fba8a56f35192449dbd3d1030f7f38cb
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-10-28 01:10:32 +0200

    Added POSIX thread wrapper and corresponding utility functions

---
+++ src/sys/thread.cpp
@@ -0,0 +1,218 @@
+/*
+ * pepper - SCM statistics report generator
+ * Copyright (C) 2010 Jonas Gehring
+ *
+ * file: sys/thread.cpp
+ * POSIX thread classes and utilities
+ */
+
+
+#include <cassert>
+
+#include <sys/time.h>
+
+#include "thread.h"
+
+
+namespace sys 
+{
+
+namespace thread
+{
+
+// Constructor
+Mutex::Mutex()
+{
+	pthread_mutex_init(&m_pmx, NULL);
+}
+
+// Destructor
+Mutex::~Mutex()
+{
+	pthread_mutex_destroy(&m_pmx);
+}
+
+// Locks the mutex
+void Mutex::lock()
+{
+	pthread_mutex_lock(&m_pmx);
+}
+
+// Unlocks the mutex
+void Mutex::unlock()
+{
+	pthread_mutex_unlock(&m_pmx);
+}
+
+
+// Constructor
+Thread::Thread()
+	: m_running(false), m_abort(false)
+{
+
+}
+
+// Starts the thread
+void Thread::start()
+{
+	assert(m_running == false);
+	m_running = true;
+	pthread_create(&m_pth, NULL, &Thread::main, this);
+}
+
+// Blocks the current thread until this thread has finished
+void Thread::wait()
+{
+	if (m_running) {
+		pthread_join(m_pth, 0);
+		m_running = false;
+	}
+}
+
+// Sets the abort flag
+void Thread::abort()
+{
+	m_mutex.lock();
+	m_abort = true;
+	m_mutex.unlock();
+}
+
+// Sleeping
+void Thread::msleep(int msecs)
+{
+	// This is from Qt-4.6, qthread_unix.cpp
+	struct timeval tv;
+	gettimeofday(&tv, 0);
+
+	timespec ti;
+	ti.tv_nsec = (tv.tv_usec + (msecs % 1000) * 1000) * 1000;
+	ti.tv_sec = tv.tv_sec + (msecs / 1000) + (ti.tv_nsec / 1000000000);
+	ti.tv_nsec %= 1000000000;
+
+	pthread_mutex_t mtx;
+	pthread_cond_t cnd;
+
+	pthread_mutex_init(&mtx, 0);
+	pthread_cond_init(&cnd, 0);
+
+	pthread_mutex_lock(&mtx);
+	(void) pthread_cond_timedwait(&cnd, &mtx, &ti);
+	pthread_mutex_unlock(&mtx);
+
+	pthread_cond_destroy(&cnd);
+	pthread_mutex_destroy(&mtx);
+}
+
+// Checks if the abort flag is set
+bool Thread::abortFlag()
+{
+	bool t;
+	m_mutex.lock();
+	t = m_abort;
+	m_mutex.unlock();
+	return t;
+}
+
+// Executs the thread's run() function
+void *Thread::main(void *obj)
+{
+	reinterpret_cast<Thread *>(obj)->run();
+	reinterpret_cast<Thread *>(obj)->m_running = false;
+	return NULL;
+}
+
+
+// Constructor
+WaitCondition::WaitCondition()
+{
+	pthread_cond_init(&m_pcond, NULL);
+}
+
+// Destructor
+WaitCondition::~WaitCondition()
+{
+	pthread_cond_destroy(&m_pcond);
+}
+
+// Blocks the current thread and waits for a signal
+void WaitCondition::wait(Mutex *mutex)
+{
+	pthread_cond_wait(&m_pcond, &mutex->m_pmx);
+}
+
+// Wakes a single waiting thread
+void WaitCondition::wake()
+{
+	pthread_cond_signal(&m_pcond);
+}
+
+// Wakes all waiting threads
+void WaitCondition::wakeAll()
+{
+	pthread_cond_broadcast(&m_pcond);
+}
+
+
+// Constructor
+Semaphore::Semaphore(int n)
+	: m_avail(n)
+{
+	pthread_mutex_init(&m_mutex, NULL);
+	pthread_cond_init(&m_cond, NULL);
+}
+
+// Destructor
+Semaphore::~Semaphore()
+{
+	pthread_mutex_destroy(&m_mutex);
+	pthread_cond_destroy(&m_cond);
+}
+
+int Semaphore::available()
+{
+	int t;
+	pthread_mutex_lock(&m_mutex);
+	t = m_avail;
+	pthread_mutex_unlock(&m_mutex);
+	return t;
+}
+
+void Semaphore::acquire(int n)
+{
+	pthread_mutex_lock(&m_mutex);
+	while (n > m_avail) {
+		pthread_cond_wait(&m_cond, &m_mutex);
+	}
+	m_avail -= n;
+	pthread_mutex_unlock(&m_mutex);
+}
+
+int Semaphore::maxAcquire(int n)
+{
+	int ac;
+	pthread_mutex_lock(&m_mutex);
+	while (1 > m_avail) {
+		pthread_cond_wait(&m_cond, &m_mutex);
+	}
+	if (m_avail > n) {
+		ac = n;
+		m_avail -= n;
+	} else {
+		ac = m_avail;
+		m_avail = 0;
+	}
+	pthread_mutex_unlock(&m_mutex);
+	return ac;
+}
+
+void Semaphore::release(int n)
+{
+	pthread_mutex_lock(&m_mutex);
+	m_avail += n;
+	pthread_cond_broadcast(&m_cond);
+	pthread_mutex_unlock(&m_mutex);
+}
+
+} // namespace thread
+
+} // namespace sys
---
+++ src/sys/thread.h
@@ -0,0 +1,107 @@
+/*
+ * pepper - SCM statistics report generator
+ * Copyright (C) 2010 Jonas Gehring
+ *
+ * file: sys/thread.h
+ * POSIX thread classes and utilities (interface)
+ */
+
+
+#ifndef SYS_THREAD_H_
+#define SYS_THREAD_H_
+
+
+#include <string>
+
+#include <pthread.h>
+#include <semaphore.h>
+
+
+namespace sys
+{
+
+namespace thread
+{
+
+class Mutex
+{
+	friend class WaitCondition;
+
+	public:
+		Mutex();
+		~Mutex();
+
+		void lock();
+		void unlock();
+
+	private:
+		pthread_mutex_t m_pmx;
+};
+
+
+class Thread
+{
+	public:
+		Thread();
+		virtual ~Thread() { }
+
+		void start();
+		void abort();
+		void wait();
+
+		static void msleep(int msecs);
+
+	protected:
+		virtual void run() = 0;
+
+		bool abortFlag();
+
+	private:
+		static void *main(void *obj);
+
+	private:
+		pthread_t m_pth;
+		volatile bool m_running;
+		Mutex m_mutex;
+		bool m_abort;
+};
+
+
+class WaitCondition
+{
+	public:
+		WaitCondition();
+		~WaitCondition();
+
+		void wait(Mutex *mutex);
+		void wake();
+		void wakeAll();
+
+	private:
+		pthread_cond_t m_pcond;
+};
+
+
+class Semaphore
+{
+	public:
+		Semaphore(int n = 0);
+		~Semaphore();
+
+		int available();
+		void acquire(int n = 1);
+		int maxAcquire(int n = 1);
+		void release(int n = 1);
+
+	private:
+		int m_avail;
+		pthread_mutex_t m_mutex;
+		pthread_cond_t m_cond;
+};
+
+} // namespace thread
+
+} // namespace sys
+
+
+#endif // SYS_THREAD_H_
