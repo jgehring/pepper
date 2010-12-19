a2fa0c965c71f936e3c7327cdf10b91796808251
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-12-19 01:55:05 +0100

    Forgot to add reports/heads.lua

---
+++ reports/heads.lua
@@ -0,0 +1,36 @@
+--[[
+-- Show repository branches and heads, similar to "git branch -v"
+--]]
+
+
+-- Script meta-data
+meta.name = "Heads"
+
+-- Main script function
+function main()
+	local repo = pepper.report.repository()
+	local branches = repo:branches()
+	local main = repo:main_branch()
+	local maxlen = 0
+	for i,v in ipairs(branches) do
+		if #v > maxlen then
+			maxlen = #v
+		end
+	end
+	maxlen = maxlen + 1
+	for i,v in ipairs(branches) do
+		local line = ""
+		if v == main then
+			line = "* " .. v
+		else
+			line = "  " .. v
+		end
+		j = #v
+		while j < maxlen do
+			line = line .. " "
+			j = j + 1
+		end
+		line = line .. repo:head(v)
+		print(line)
+	end
+end
