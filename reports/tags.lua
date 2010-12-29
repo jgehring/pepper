d5b10203d15d324a2931e2042d242222419c6935
Jonas Gehring <jonas.gehring@boolsoft.org>
2010-12-29 23:24:26 +0100

    Added sample report for showing repository tags

---
+++ reports/tags.lua
@@ -0,0 +1,28 @@
+--[[
+	Shows all tags
+--]]
+
+
+-- Script meta-data
+meta.name = "Tags"
+
+-- Main script function
+function main()
+	local repo = pepper.report.repository()
+	local tags = repo:tags()
+	local maxlen = 0
+	for i,v in ipairs(tags) do
+		if #v:name() > maxlen then
+			maxlen = #v:name()
+		end
+	end
+	maxlen = maxlen + 2
+	for i,v in ipairs(tags) do
+		local line = v:name()
+		while #line < maxlen do
+			line = line .. " "
+		end
+		line = line .. v:id()
+		print(line)
+	end
+end
