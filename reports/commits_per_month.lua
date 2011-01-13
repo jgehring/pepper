36daa46155ab6a3112f21a8824f4b751f28362c7
Jonas Gehring <jonas.gehring@boolsoft.org>
2011-01-13 13:50:25 +0100

    Fixed dates in commits_per_month report

--- reports/commits_per_month.lua
+++ reports/commits_per_month.lua
@@ -13,7 +13,7 @@ meta.options = {{"-bARG, --branch=ARG", "Select branch"},
 function callback(r)
 	local date = os.date("*t", r:date())
 	-- Ignore commits older than 1 year
-	if (now["year"] - date["year"] > 1) or (now["year"] - date["year"] == 1 and now["month"] <= date["month"]) then
+	if (now["year"] - date["year"] > 1) or (now["year"] - date["year"] == 1 and date["month"] <= now["month"]) then
 		return
 	end
 
@@ -39,6 +39,7 @@ function main()
 	-- Generate a data file
 	local names = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
 	local month = (now["month"] % 12) + 1
+	local i = 1
 	local file, filename = pepper.utils.mkstemp()
 	while true do
 		ncommits = 0
@@ -47,11 +48,12 @@ function main()
 			ncommits = commits[month]
 			nchanges = changes[month]
 		end
-		file:write(month .. " " .. names[month] .. " " .. ncommits .. " " .. nchanges .. "\n")
+		file:write(i .. " " .. names[month] .. " " .. ncommits .. " " .. nchanges .. "\n")
 		if month == now["month"] then
 			break
 		end
 		month = (month % 12) + 1
+		i = i + 1
 	end
 
 	-- Generate graphs
@@ -68,8 +70,24 @@ set key box
 set key below
 set boxwidth 0.4
 set xtics nomirror
-set xtics ("January" 1, "March" 3, "May" 5, "July" 7, "September" 9, "November" 11) scale 0
 ]])
+
+	-- Determine xtics
+	month = (now["month"] % 12) + 1
+	i = 1
+	local xtics = ""
+	while true do
+		if month == now["month"] then
+			break
+		end
+		if i % 2 ~= 0 then
+			xtics = xtics .. '"' .. names[month] .. "\" " .. i .. ","
+		end
+		month = (month % 12) + 1
+		i = i + 1
+	end
+	plot:cmd("set xtics (" .. xtics:sub(0, #xtics-1) .. ") scale 0")
+
 	local cmd = "plot "
 	cmd = cmd .. "\"" .. filename .. "\" using 1:3 with boxes fs solid title \"Commits\", "
 	cmd = cmd .. "\"" .. filename .. "\" using ($1+.4):4 with boxes fs solid title \"Changes\";"
