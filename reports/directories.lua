3e83052affa1258d6e8a3e11bcf7fab1530b6230
Jonas Gehring <jonas.gehring@boolsoft.org>
2011-08-31 00:52:38 +0200

    Fixed indentation in directories report

--- reports/directories.lua
+++ reports/directories.lua
@@ -1,12 +1,12 @@
 --[[
-    pepper - SCM statistics report generator
-    Copyright (C) 2010-2011 Jonas Gehring
+	pepper - SCM statistics report generator
+	Copyright (C) 2010-2011 Jonas Gehring
 
-    Released under the GNU General Public License, version 3.
-    Please see the COPYING file in the source distribution for license
-    terms and conditions, or see http://www.gnu.org/licenses/.
+	Released under the GNU General Public License, version 3.
+	Please see the COPYING file in the source distribution for license
+	terms and conditions, or see http://www.gnu.org/licenses/.
 
-    file: diffstat.lua
+	file: diffstat.lua
 	Visualizes directory size changes on a given branch.
 --]]
 
